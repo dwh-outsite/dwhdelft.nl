806b25314d776e9b22370e5997b435166db47427
Casper Boone <mail@casperboone.nl>
2019-07-13 18:32:05 +0200

    Add Outsite logo

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <nav class="absolute z-50 w-full mt-8">
-      <div class="container mx-auto flex justify-between">
-        <img src="icon.png" alt="Outsite" class="w-10 h-10" />
+      <div class="container mx-auto flex justify-between items-center">
+        <img src="~assets/images/outsite_logo.svg" alt="Outsite" class="h-16" />
         <div>
           <nuxt-link to="/" class="text-white text-xl font-semibold no-underline mr-4">
             Home

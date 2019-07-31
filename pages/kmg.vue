b8a2b55f26c2556213228d14657cc299336a0d61
Casper Boone <mail@casperboone.nl>
2019-07-31 17:43:28 +0200

    Add description and style form

--- pages/kmg.vue
+++ pages/kmg.vue
@@ -1,56 +1,53 @@
 <template>
   <div>
     <header>
-      <Header>
+      <Header small="true">
         <h1 class="text-4xl text-white font-normal">
-          {{ $t('hero.title') }}
+          {{ $t('ways_to_join.kmg.title') }}
         </h1>
-        <h2 class="text-2xl text-white mt-2 w-1/2 font-light">
-          {{ $t('hero.subtitle') }}
-        </h2>
-        <div class="mt-8 flex">
-          <button class="block bg-pink-400 px-4 py-2 rounded text-white uppercase font-semibold tracking-wider shadow">
-            Join Outsite
-          </button>
-          <button
-            class="block ml-4 bg-white px-4 py-2 rounded text-gray-700 uppercase tracking-wider shadow font-semibold"
-          >
-            Watch video
-          </button>
-        </div>
       </Header>
     </header>
 
-    <section>
-      <form name="contact" method="POST" data-netlify="true">
-        <p>
-          <label>
-            Your Name:
-            <input type="text" name="name" class="border" />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your Email:
-            <input type="email" name="email" class="border" />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your Role:
-            <select name="role[]" multiple class="border">
-              <option value="leader">Leader</option>
-              <option value="follower">Follower</option>
-            </select>
-          </label>
-        </p>
-        <p>
-          <label>Message: <textarea name="message" class="border"></textarea></label>
-        </p>
-        <p>
-          <button type="submit" class="border">Send</button>
-        </p>
-      </form>
+    <section class="container mx-auto text-center pb-20 text-2xl leading-normatext-grey-darkest">
+      <p class="px-64 mb-8">{{ $t('ways_to_join.kmg.description') }}</p>
+      <p class="px-64">De volgende KMG start in: <strong>september 2019</strong></p>
+    </section>
+
+    <section class="bg-gray-200 pt-12 pb-12">
+      <div class="mx-auto container">
+        <h2 class="tracking-wide font-semibold uppercase text-2xl mb-12 mx-2 text-center">Aanmelden voor de KMG</h2>
+        <form class="w-2/3 mx-auto" name="kmg" method="POST" data-netlify="true">
+          <p class="form-element">
+            <label>Naam</label>
+            <input type="text" name="name" placeholder="Naam" required />
+          </p>
+          <p class="form-element">
+            <label>E-mailadres</label>
+            <input type="email" name="email" placeholder="E-mailadres" required />
+          </p>
+          <p class="form-element">
+            <label>Geboortedatum</label>
+            <input type="text" name="dateofbirth" placeholder="Geboortedatum" required />
+          </p>
+          <p class="form-element">
+            <label>Telefoonnummer</label>
+            <input type="text" name="phonenumber" placeholder="Telefoonnummer" />
+          </p>
+          <p class="form-element">
+            <label>Woonplaats</label>
+            <input type="text" name="city" placeholder="Woonplaats" />
+          </p>
+          <p class="form-element">
+            <label>Heb je verder nog vragen?</label>
+            <textarea name="remarks" placeholder="Opmerkingen of vragen"></textarea>
+          </p>
+          <p class="my-8 text-right">
+            <button class="bg-pink-400 px-4 py-2 rounded text-white uppercase font-semibold tracking-wider shadow">
+              Aanmelden
+            </button>
+          </p>
+        </form>
+      </div>
     </section>
 
     <footer class="bg-gray-700">
@@ -91,16 +88,16 @@ export default {
 </script>
 
 <style>
-.information {
-  @apply relative;
+.form-element {
+  @apply mb-4;
 }
 
-.information::before {
-  @apply bg-gray-200 absolute w-full;
-  height: 250%;
-  transform: skewY(-13deg);
-  content: '';
-  z-index: -1;
-  top: 0px;
+.form-element label {
+  @apply uppercase text-sm tracking-wide font-semibold block mb-1;
+}
+
+.form-element input,
+.form-element textarea {
+  @apply bg-white rounded-lg block px-4 py-3 w-full;
 }
 </style>

07f9c42eee9d459cdf9c40107d9b6395bb8be32a
Casper Boone <mail@casperboone.nl>
2019-08-05 00:02:57 +0200

    Add favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,13 +9,19 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: pkg.name,
+    title: 'Outsite - LHBT+ vereniging Delft',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+      { rel: 'manifest', href: '/site.webmanifest' },
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#e31c79' },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' }
+    ]
   },
 
   /*

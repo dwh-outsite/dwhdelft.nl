930ed41766995ec62a2d11d968960be7564517c8
Casper Boone <mail@casperboone.nl>
2019-11-16 16:45:43 +0100

    Use Google Drive API for prettier Andersblad archive
    This API key is safe to share publicly.

--- pages/andersblad.vue
+++ pages/andersblad.vue
@@ -19,12 +19,21 @@
         <div class="text-center mb-6">
           <h1 class="text-white font-medium text-5xl" v-html="$t('andersblad.list_title')" />
         </div>
-        <div class="shadow-xl p-6 rounded-lg bg-white md:w-2/3 mx-auto">
-          <iframe
-            src="https://drive.google.com/embeddedfolderview?id=1z_V_JSdyxX9jGH8CmC1ahMCtRk4tlzfM"
-            class="w-full"
-            height="800"
-          />
+        <div class="flex flex-wrap justify-center">
+          <div v-for="edition in editions" :key="edition.id" class="md:w-1/3 p-4">
+            <a :href="edition.webViewLink" target="_blank">
+              <div class="shadow-xl p-6 rounded-lg bg-white hover:bg-purple-100 hover:text-purple-500 flex">
+                <div>
+                  <img :src="edition.thumbnailLink" class="h-48" />
+                </div>
+                <div class="flex-1 ml-4 flex flex-col justify-center">
+                  <h3 class="text-2xl font-semibold">{{ edition.name }}</h3>
+                  <h4 class="text-xl">{{ edition.title }}</h4>
+                  <p class="text-lg">{{ edition.publishDate }}</p>
+                </div>
+              </div>
+            </a>
+          </div>
         </div>
       </div>
     </section>
@@ -32,11 +41,38 @@
 </template>
 
 <script>
+import axios from 'axios'
 import Header from '~/components/Header'
 
 export default {
   components: {
     Header
+  },
+  data() {
+    return {
+      editions: []
+    }
+  },
+  mounted() {
+    axios
+      .get(
+        `https://www.googleapis.com/drive/v3/files` +
+          `?orderBy=name desc` +
+          `&q='1z_V_JSdyxX9jGH8CmC1ahMCtRk4tlzfM'%20in%20parents` +
+          `&fields=files(id, name, webContentLink, webViewLink, thumbnailLink)` +
+          `&key=AIzaSyDwi_l2R3qDWkh2HN8_AmIpy7mk8Ij7nk8`
+      )
+      .then(response => {
+        this.editions = response.data.files.map(file => {
+          return {
+            ...file,
+            filename: file.name,
+            name: file.name.substr(0, file.name.indexOf(',')),
+            title: file.name.slice(file.name.indexOf(', ') + 2, file.name.lastIndexOf(',')),
+            publishDate: file.name.slice(file.name.lastIndexOf(',') + 1, -4)
+          }
+        })
+      })
   }
 }
 </script>

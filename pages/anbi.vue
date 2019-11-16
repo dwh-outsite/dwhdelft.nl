1fa811bf711da9e1e615e82f6496b13abfb94f28
Casper Boone <mail@casperboone.nl>
2019-11-16 21:55:08 +0100

    Fix js issues ANBI page

--- pages/anbi.vue
+++ pages/anbi.vue
@@ -10,12 +10,12 @@
 
     <section class="container mx-auto  text-xl md:text-2xl leading-normal text-gray-800">
       <div class="md:w-2/3 mx-4 md:mx-auto">
-        <p class="mt-8 md:mt-0 mb-8" v-html="$t('anbi.main_text')" />
-        <p class="mb-4" v-html="$t('anbi.annual_report')" />
+        <div class="mt-8 md:mt-0 mb-8" v-html="$t('anbi.main_text')" />
+        <div class="mb-4" v-html="$t('anbi.annual_report')" />
         <FilesList folderId="1SEhPdLmDC-MxwcqiRnaMlAcvoPgnq16a" />
-        <p class="mb-4" v-html="$t('anbi.annual_plan')" />
+        <div class="mb-4" v-html="$t('anbi.annual_plan')" />
         <FilesList folderId="1Y_3eX7L6RKOnFMCi1m3YP4Dkwc9Ibw2Y" />
-        <p class="mb-4" v-html="$t('anbi.financial')" />
+        <div class="mb-4" v-html="$t('anbi.financial')" />
         <FilesList folderId="1wOt93uBOFba4lHrf6In1YZJPzT4L9Doi" />
       </div>
     </section>

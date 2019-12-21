6359ee9604211e49ea343a403c5e216d346f0b87
Casper Boone <mail@casperboone.nl>
2019-12-22 00:40:51 +0100

    Convert board member names to images

--- components/BoardMember.vue
+++ components/BoardMember.vue
@@ -1,12 +1,37 @@
 <template>
   <div class="leading-tight mb-6 md:w-1/2">
-    <strong>{{ name }}</strong> | {{ role }}<br />
+    <img :src="nameAsImage(name, role)" />
     <span class="text-gray-400">{{ email }}</span>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['name', 'role', 'email']
+  props: ['name', 'role', 'email'],
+  methods: {
+    nameAsImage(name, role) {
+      const image = `
+        <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="20">
+          <foreignObject width="100%" height="100%" x="0" y="0" externalResourcesRequired="true">
+          <span xmlns="http://www.w3.org/1999/xhtml" style="
+            color: rgb(255, 255, 255);
+            font: 16px / 20px &quot;Source Sans Pro&quot;, -apple-system, system-ui, &quot;Segoe UI&quot;, 
+                  Roboto, &quot;Helvetica Neue&quot;, Arial, sans-serif;
+            font-kerning: auto;
+            font-optical-sizing: auto;
+            word-spacing: 1px;
+            ">
+            <strong>${name}</strong>
+            | ${role}
+          </span>
+        </foreignObject>
+      </svg>
+      `
+
+      const base64Encoded = Buffer.from(image).toString('base64')
+
+      return `data:image/svg+xml;charset=utf-8;base64,${base64Encoded}`
+    }
+  }
 }
 </script>

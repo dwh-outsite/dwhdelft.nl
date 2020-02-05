e16ad99c7dd5533558e5f519ee3db21457f01402
Casper Boone <mail@casperboone.nl>
2020-02-05 01:22:18 +0100

    Fix event cover image aspect ratio

--- components/Activities.vue
+++ components/Activities.vue
@@ -13,9 +13,10 @@
           class="flex flex-col justify-between h-full"
         >
           <div>
-            <div class="aspect-ratio-fb relative">
-              <img :src="activity.cover.source" class="rounded-t absolute w-full h-full" />
-            </div>
+            <div
+              :style="'background-image: url(' + activity.cover.source + ');'"
+              class="aspect-ratio-fb relative overflow-hidden bg-cover bg-center"
+            />
             <div class="px-4 pt-3">
               <h3 class="text-purple-500 text-xl font-bold">{{ activity.name }}</h3>
             </div>

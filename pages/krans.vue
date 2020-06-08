f4fbdb70492d3d6f844fe842ff73567f7f67e07b
Casper Boone <mail@casperboone.nl>
2020-06-08 17:25:19 +0200

    Remove last name from wreath-laying page

--- pages/krans.vue
+++ pages/krans.vue
@@ -124,7 +124,7 @@ export default {
   },
   data() {
     return {
-      dwhBoard: ['Robert Baart', 'Nick van Loo', 'Quinten Star', 'Casper Boone', 'Jelle Vos', 'Jeroen Wegdam'],
+      dwhBoard: ['Robert Baart', 'Nick van Loo', 'Quinten Star', 'Casper Boone', 'Jelle Vos', 'Jeroen'],
       outsiteBoard: ['Niv Bharos', 'Ivo Brands', 'Bouke Stoelinga', 'Eva Pelk'],
       participants: []
     }

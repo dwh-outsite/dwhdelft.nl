1bc41d7d5ff7759c7b3a08173a5a18898f3a8633
Casper Boone <mail@casperboone.nl>
2020-06-10 15:22:35 +0200

    debug 7

--- pages/book.vue
+++ pages/book.vue
@@ -9,38 +9,6 @@
         <h1 v-text="$t('bookings.title')" class="text-4xl text-white font-normal mt-2" />
       </Header>
     </header>
-
-    <section class="container mx-auto relative pt-12 md:pt-6 px-4">
-      <div class="pb-16 m-auto">
-        <h2
-          v-html="$t('bookings.description_title')"
-          class="text-purple-400 leading-none text-5xl mb-6 md:mb-12 md:text-6xl"
-        />
-        <p v-html="$t('bookings.description')" class="text-xl md:text-2xl leading-normal text-gray-800 md:mb-12" />
-      </div>
-    </section>
-
-    <section class="information relative pb-12 md:pb-20">
-      <div class="mx-auto container px-4 md:flex flex-row-reverse">
-        <div class="flex-1 pt-16 md:pl-16">
-          <h2
-            v-html="$t('bookings.form_information.title')"
-            class="text-white leading-none text-5xl mb-6 md:text-6xl"
-          />
-          <p v-text="$t('bookings.form_information.description')" class="text-white text-xl" />
-          <div class="my-4 text-xl">
-            <div
-              v-for="rule in $t('bookings.form_information.rules')"
-              :key="rule"
-              v-html="rule"
-              class="bg-purple-400 text-white rounded p-4 mb-2"
-            />
-          </div>
-          <div v-text="$t('bookings.form_information.rules_disclaimer')" class="text-sm text-white" />
-        </div>
-        <div class="md:w-1/2 pt-8 md:pt-40"></div>
-      </div>
-    </section>
   </div>
 </template>
 

cfce606f75fa938cc3db76fa3b6c704f975d13dd
Robert Baart <robert.baart@gmail.com>
2020-08-28 11:56:53 +0200

    Add place for the chats to go

--- pages/chatgroepen.vue
+++ pages/chatgroepen.vue
@@ -19,62 +19,17 @@
         <div class="text-center mb-6">
           <h1 v-html="$t('chatgroepen.general_title')" class="text-white font-medium text-5xl" />
         </div>
-        <div class="flex flex-wrap justify-center">
-          <div v-for="edition in editions" :key="edition.id" class="md:w-1/3 p-4">
-            <a :href="edition.webViewLink" target="_blank">
-              <div class="shadow-xl p-6 rounded-lg bg-white hover:bg-purple-100 hover:text-purple-500 flex">
-                <div>
-                  <img :src="edition.thumbnailLink" class="h-48" />
-                </div>
-                <div class="flex-1 ml-4 flex flex-col justify-center">
-                  <h3 class="text-2xl font-semibold">{{ edition.name }}</h3>
-                  <h4 class="text-xl">{{ edition.title }}</h4>
-                  <p class="text-lg text-gray-600">{{ edition.publishDate }}</p>
-                </div>
-              </div>
-            </a>
-          </div>
-        </div>
+        <div class="flex flex-wrap justify-center"></div>
 
         <div class="text-center mb-6">
           <h1 v-html="$t('chatgroepen.volunteer_title')" class="text-white font-medium text-5xl" />
         </div>
-        <div class="flex flex-wrap justify-center">
-          <div v-for="edition in editions" :key="edition.id" class="md:w-1/3 p-4">
-            <a :href="edition.webViewLink" target="_blank">
-              <div class="shadow-xl p-6 rounded-lg bg-white hover:bg-purple-100 hover:text-purple-500 flex">
-                <div>
-                  <img :src="edition.thumbnailLink" class="h-48" />
-                </div>
-                <div class="flex-1 ml-4 flex flex-col justify-center">
-                  <h3 class="text-2xl font-semibold">{{ edition.name }}</h3>
-                  <h4 class="text-xl">{{ edition.title }}</h4>
-                  <p class="text-lg text-gray-600">{{ edition.publishDate }}</p>
-                </div>
-              </div>
-            </a>
-          </div>
-        </div>
+        <div class="flex flex-wrap justify-center"></div>
 
         <div class="text-center mb-6">
           <h1 v-html="$t('chatgroepen.interests_title')" class="text-white font-medium text-5xl" />
         </div>
-        <div class="flex flex-wrap justify-center">
-          <div v-for="edition in editions" :key="edition.id" class="md:w-1/3 p-4">
-            <a :href="edition.webViewLink" target="_blank">
-              <div class="shadow-xl p-6 rounded-lg bg-white hover:bg-purple-100 hover:text-purple-500 flex">
-                <div>
-                  <img :src="edition.thumbnailLink" class="h-48" />
-                </div>
-                <div class="flex-1 ml-4 flex flex-col justify-center">
-                  <h3 class="text-2xl font-semibold">{{ edition.name }}</h3>
-                  <h4 class="text-xl">{{ edition.title }}</h4>
-                  <p class="text-lg text-gray-600">{{ edition.publishDate }}</p>
-                </div>
-              </div>
-            </a>
-          </div>
-        </div>
+        <div class="flex flex-wrap justify-center"></div>
       </div>
     </section>
   </div>

947392fef0ccd02a6db7dbbed8abeb88a94d1495
Joel Abrahams <abrahamsjo@gmail.com>
2020-09-01 22:25:58 +0200

    Update text
    - Update description english chat groups
    - Update description of thursday and saturday bar
      volunteers

--- lang/en_chat_groups.js
+++ lang/en_chat_groups.js
@@ -9,43 +9,43 @@ export default {
       tends to have more younger members.
     `,
     Announcements: `
-      Don't want to be in the busy Outsite chat but like updates about upcoming youth events? Join the announcements 
+      Don't want to be in the busy Outsite chat but like updates about upcoming youth events? Join the announcements
       chat to receive just those and nothing else.
     `,
     Discord: `
-      Want to talk to some people outside of our regular events? Play a game or watch a show together, the discord is 
+      Want to talk to some people outside of our regular events? Play a game or watch a show together, the discord is
       the place to talk.
     `
   },
   'Groups for <strong>volunteers</strong>': {
     'Thursday bar': `
-      Bartending is one of the funnest things to do, you get to talk to all the people who visit and everyone loves you
+      Bartending is one of the most fun things to do, you get to talk to all the people who visit and everyone loves you
       as you're bringing them new drinks! Join this chat if you'd like to bartend on Thursday.
     `,
     'Saturday bar': `
-      Bartending is one of the funnest things to do, you get to talk to all the people who visit and everyone loves you
+      Bartending is one of the most fun things to do, you get to talk to all the people who visit and everyone loves you
       as you're bringing them new drinks! Join this chat if you'd like to bartend on Saturday.
     `,
     EatingOUT: `
-      The chat for everyone interested in cooking on Tuesdays. Are you a kitchen pro or would you like to be? Join the 
+      The chat for everyone interested in cooking on Tuesdays. Are you a kitchen pro or would you like to be? Join the
       chat and cook for a crowd, on your own or with someone more experienced.
     `
   },
   'Groups for <strong>various interests</strong>': {
     'Chicks before Ducks': `
-      If you are interested and identify anything chick-ish you can join our group 😊 (decide for yourself we don’t 
-      shutOUT anyone) 
+      If you are interested and identify anything chick-ish you can join our group 😊 (decide for yourself we don’t
+      shutOUT anyone)
     `,
     ChocOUT: `
       For all you chocolate and milkshake lovers. Here we discuss everything chocolate, and sometimes we go to Quetzal,
       the chocolate bar.
     `,
     'Darkrooms & Daddies': `
-      Do you like Dungeons & Dragons or similar roleplaying games? This is the chat for all the alignment memes you 
+      Do you like Dungeons & Dragons or similar roleplaying games? This is the chat for all the alignment memes you
       could ever want and a place to find new people to play with.
     `,
     'Gender Blender': `
-      Are you trans, non-binary or gender non-conforming? Join the Gender Blender and talk all about binders, the 
+      Are you trans, non-binary or gender non-conforming? Join the Gender Blender and talk all about binders, the
       spinniest of dresses, or all the weird stuff cis people asign gender to, all among friends.
     `,
     Minecraft: `
@@ -53,11 +53,11 @@ export default {
       there's something to do for you on our Minecraft server, and this chat is where we find people to play with.
     `,
     'Politics & Discussion': `
-      If you like to discuss the latest in queer and general politics, discuss topics for hours or are looking for 
-      people to attend a protest with, this is the place to be. 
+      If you like to discuss the latest in queer and general politics, discuss topics for hours or are looking for
+      people to attend a protest with, this is the place to be.
     `,
     Strijders: `
-      Studying can be hard, especially on your own. In this group we plan to study together, at the TU or now in our 
+      Studying can be hard, especially on your own. In this group we plan to study together, at the TU or now in our
       own bar, and even just the fancy coffee breaks are worth it :)
     `
   }

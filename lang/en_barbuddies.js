866913b158be6734e411bac9f4a56bdb381c4119
Robert Baart <robert.baart@gmail.com>
2020-11-20 11:13:22 +0100

    Updated barbuddy texts
    - Changed name for Dante
    - Updated ages
    - Made use of association name consistent
    - Remove Daan, Pedro and add Carlos

--- lang/en_barbuddies.js
+++ lang/en_barbuddies.js
@@ -1,22 +1,19 @@
 export default {
-  Daan: `
-  Hey hey, I'm Daan. Twenty years old and currently doing my bachelor Electrical Engineering here in Delft. I have been
-  visiting DWH/Outsite for just over a year now, but in this one year I have had tons of fun on the bar nights and other
-  activities organized by the association. It can be difficult to get the courage to finally visit a bar like ours, and 
-  I know this from experience. I hope that I can help you make this first step. As a barbuddy I would like to introduce
-  you to all different kinds of people that visit our association on an evening. I can also tell you more about our
-  activities, so that maybe one day we will see you again, this time without a barbuddy :)
+  Andreas: `
+  Hey, I’m Andreas, in my mid twenties and an Aerospace student. I am a very active member of DWH and I love
+  meeting new people and showing them our society. For me it is a place where you can be yourself and can participated
+  in all sorts of fun activities.
   `,
   Jeff: `
-  Hi, my name is Jeff. I'm in my late twenties, and I study Computer Science at TU Delft. I've been a member of Outsite
+  Hi, my name is Jeff. I'm in my late twenties, and I study Computer Science at TU Delft. I've been a member of DWH
   for a few years now. I'm a regular at the EatingOUT on Tuesdays, but I'm happy to be your barbuddy on a Thursday. I
   remember how jittery I was walking into the bar for the first time, and that was for the introduction group. That
-  turned out to be a great decision though, because DWH/Outsite is just a really nice place with lovely people and fun
+  turned out to be a great decision though, because DWH is just a really nice place with lovely people and fun
   activities.
   `,
   Mariana: `
-  Hello! I’m Mariana, I’m in my late twenties, from Portugal and I have been living in the Netherlands for 6 years. I
-  studied here at TU Delft and then I stayed for work. I joined DWH early on and was on the board of Outsite for a year
+  Hello! I’m Mariana, I’m in my early thirties, from Portugal and I have been living in the Netherlands for 6 years. I
+  studied here at TU Delft and then I stayed for work. I joined DWH early on and was on the board of DWH for a year
   and I currently help with the women’s events. I’ve met many good friend through DWH and I’m always glad to meet new
   people and introduce them to DWH since it helped me so much when I came to the Netherlands.
   `,
@@ -27,46 +24,48 @@ export default {
   because it’s a nice spot to hang out and I’ve met a lot of new and interesting people within this association.
   `,
   Govert: `
-  Hello I’m Govert, at the end of my twenties. I study architecture and I’m a teacher a high school. I’m a very active
+  Hello I’m Govert, at the start of my thirties. I study architecture and I’m a teacher a high school. I’m a very active
   volunteer within DWH. In my opinion, DWH stands for a niceand really diverse group of people. A place to organize lots
   of things and also a place to learn a lot about yourself. I really like meeting new people and introducing them to
   this awesome association.
   `,
-  Andreas: `
-  Hey, I’m Andreas, in my early twenties and an Aerospace student. I am a very active member of DWH/Outsite and I love
-  meeting new people and showing them our society. For me it is a place where you can be yourself and can participated
-  in all sorts of fun activities.
+  Niv: `
+  Hey! I’m Niv, 22 yo and studying Applied Physics. In the meantime, I found my place in this cool association (I’m a
+  KMG parent and I organise the OWee this year). When I first stepped foot into the building, I was so nervous! I soon
+  met some really nice and special people, which I consider to be my close friends now. If you feel nervous about coming
+  to DWH, I totally get it; that’s how we all felt the first time. I hope I can make it easier for you!
   `,
-  Pedro: `
-  Hola! My name is Pedro and I am in my early twenties. I am currently studying Microelectronics at TU-Delft, but my
-  real passion outside of university is cooking. Over the past year I have been very involved at DWH helping with events
-  and volunteering as a bartender. I would be very happy to introduce you to the association and its amazing/fabulous
-  members (and maybe I can share a couple of recipes or comment on the last RPDR episode while doing so). You won’t
-  regret coming to DWH for a drink, a dance, or just a chat, and you will feel the love and support of our beautiful
-  community.
+  Carlos: `
+  Hey hola! I'm Carlos, a 24-year-old Spaniard, studying MSc Design for Interaction. I've been in DWH for a bit 
+  over three years now, and have found not only good people; but also an amazing opportunity. An opportunity to 
+  exchange experiences and enrich each other's lives, a way to get to know people outside your own bubble, your own 
+  country, your own culture. Also bartending is just fun af. I can introduce you to the grooviest (do folks say that 
+  anymore?) people; and have long (and fiery) debates about activism, art or music. Or basically any other thing. If 
+  you're feeling isolated or insecure, I think coming to DWH and letting yourself be warmly welcomed is one of 
+  the best things you can do.
   `,
   Wessel: `
-  Heya! I'm Wessel, 24 years old and I study Aerospace engineering. When I joined Outsite by joining the KMG, the
+  Heya! I'm Wessel, 24 years old and I study Aerospace engineering. When I joined DWH by joining the KMG, the
   enthusiasm coming from the members and the open and loving atmosphere made me feel at home straight away! I also
   started helping with parties and other activities and by doing so I got to know the association well pretty quickly
   and saw the power in our diverse association, everybody chipping in with their talents which results in very awesome
-  activities! For me Outsite feels as one big family where everybody has their own cozy place and I would love to help
-  you find yours at Outsite!
+  activities! For me DWH feels as one big family where everybody has their own cozy place and I would love to 
+  help you find yours at DWH!
   `,
   Hidde: `
-  Hey, I am Hidde. I am 23 years old and studying for a master’s degree in Applied Mathematics, the probability track.
-  I first came to Outsite around 3 years ago, but it wasn’t until last year that I became a lot more active; I am often
-  a volunteer during events, joined various committees and am now also a barbuddy. This is a huge difference with the
-  first few years that I visited Outsite. I often sat alone and didn’t talk much with other people, since I didn’t
-  really feel like I knew them. This is where a barbuddy comes in! As your barbuddy, I can introduce you to all kinds of
-  people from Outsite, including myself of course, and make sure you get to know them and they get to know you. Then the
-  next time you decide to visit, you won’t feel like you’re standing in between a group of strangers, but hopefully will
-  see some nice, familiar faces.
+  Hey, I am Hidde. I am 24 years old and studying for a master’s degree in Applied Mathematics, the probability track.
+  I first came to DWH around 3 years ago, but it wasn’t until last year that I became a lot more active; I am 
+  often a volunteer during events, joined various committees and am now also a barbuddy. This is a huge difference with 
+  the first few years that I visited DWH. I often sat alone and didn’t talk much with other people, since I
+  didn’t really feel like I knew them. This is where a barbuddy comes in! As your barbuddy, I can introduce you to all 
+  kinds of people from DWH, including myself of course, and make sure you get to know them and they get to know 
+  you. Then the next time you decide to visit, you won’t feel like you’re standing in between a group of strangers, but 
+  hopefully will see some nice, familiar faces.
   `,
   Matthew: `
   Heyyy I am Matthew, a Palestinian boy in his mid-twenties doing his masters in Microelectronics. When TU is not
   consuming my soul I enjoy reading (fiction and non-finction), exercising (regular gym goer, picking up running and
   bouldering), gaming (board and video games, obsessed with escape rooms), and TV Shows (random Netflix and Rupauls). I
-  am happy you decided to come to Outsite, see you there!
+  am happy you decided to come to DWH, see you there!
   `
 }
--- lang/nl_barbuddies.js
+++ lang/nl_barbuddies.js
@@ -1,19 +1,9 @@
 export default {
-  Daan: `
-  Hey hey, ik ben Daan. Twintig jaar oud en momenteel bezig met mijn bachelor Electrical Engineering hier in Delft. 
-  Zelf kom ik eigenlijk pas net meer dan een jaar op deze vereniging, maar ik heb in dat ene jaar erg veel plezier
-  beleefd aan de baravonden en andere activiteiten van DWH/Outsite.
-  Ik weet zelf hoe spannend het kan zijn om voor het eerst een bar als de onze binnen te stappen, en ik hoop dat ik
-  je kan helpen om die stap te durven zetten.
-  Als barbuddy stel ik je graag voor aan alle verschillende soorten mensen die onze vereniging zo op een avond komen
-  bezoeken. Ook kan ik je veel meer vertellen over wat wij als vereniging zoal doen qua activiteiten en wie weet zien
-  we je dan ook zonder barbuddy weer eens terug :)
-  `,
   Jeff: `
   Hoi, mijn naam is Jeff. Ik ben achter in de twintig en ik studeer Technische Informatica aan de TU Delft. Ik ben al
-  een aantal jaar lid bij Outsite. Ik kom vaak naar de EatingOUT op dinsdagen, maar ik vind het ook leuk om op een
+  een aantal jaar lid bij DWH. Ik kom vaak naar de EatingOUT op dinsdagen, maar ik vind het ook leuk om op een
   donderdag langs te komen als barbuddy. Ik herinner me hoe spannend ik het vond toen ik voor het eerste de bar
-  binnenkwam, en dat was voor de Kennismakingsgroep. Dat bleek echter een goede beslissing te zijn, want DWH/Outsite
+  binnenkwam, en dat was voor de Kennismakingsgroep. Dat bleek echter een goede beslissing te zijn, want DWH
   is een superfijne plek met lieve mensen en leuke activiteiten.
   `,
   Dennis: `
@@ -23,19 +13,27 @@ export default {
   leren kennen en mijn vriendengroep heb uitgebreid.
   `,
   Govert: `
-  Hoi ik ben Govert, eind twintig. Momenteel student bouwkunde en ik geef les op een middelbare school. Zeer actief
+  Hoi ik ben Govert, begin dertig. Momenteel student bouwkunde en ik geef les op een middelbare school. Zeer actief
   binnen DWH. DWH is voor mij een leuke groep mensen, heel divers. En ook een plek om allerlei dingen te organiseren en
   ook om over jezelf te leren. Ik kom er al een aantal jaren met heel veel plezier. Ik vind het heel leuk om nieuwe
   mensen kennis te laten maken met de vereniging en de mensen die er rondlopen.
   `,
   Andreas: `
-  Hey, ik ben Andreas, begin twintig en in mijn bachelor Lucht- & Ruimtevaarttechniek. Ik ben erg actief binnen
-  DWH/Outsite en vind het superleuk nieuwe mensen te leren kennen en ze onze vereniging te laten zien. Voor mij is het
+  Hey, ik ben Andreas, mid twintig en in mijn bachelor Lucht- & Ruimtevaarttechniek. Ik ben erg actief binnen
+  DWH en vind het superleuk nieuwe mensen te leren kennen en ze onze vereniging te laten zien. Voor mij is het
   een plek waar je jezelf kan zijn en waar allerlei soorten activiteiten worden georganiseerd waar je aan kunt
   deelnemen.
   `,
+  Niv: `
+  Hey! Ik ben Niv, 22 en bezig met mijn bachelor Technische Natuurkunde. Inmiddels heb ik helemaal mijn plekje
+  gevonden bij DWH (kmg ouder en organiseer dit jaar de OWee), maar in het begin was dat zeker nog spannend. Ik 
+  leerde al snel mensen kennen, die ik nu als hele goede vrienden beschouw. De bijzondere en aardige mensen bij de 
+  vereniging maken het ontzettend leuk! Als je het nog een beetje spannend vindt om langs te komen, begrijp ik dat 
+  volkomen; dat hadden we allemaal toen we voor het eerst het pand binnen stapten. Hopelijk kan ik het als barbuddy 
+  iets makkelijker voor je maken!
+  `,
   Wessel: `
-  Hey! Ik ben Wessel, 24 jaar en ik studeer Lucht- en Ruimtevaart Techniek. Toen ik via de KMG Outsite binnenkwam
+  Hey! Ik ben Wessel, 24 jaar en ik studeer Lucht- en Ruimtevaart Techniek. Toen ik via de KMG DWH binnenkwam
   werd ik gelijk zo enthousiast gemaakt door de open sfeer en lieve mensen dat ik gelijk na de KMG me vol ben gaan
   inzetten voor de vereniging! Ik voelde me echt thuis en door iets te doen voor de vereniging leerde ik heel gauw de
   hele vereniging kennen en ook de unieke talenten die elk lid heeft, waardoor je met iedereen bij elkaar echt
@@ -45,33 +43,33 @@ export default {
   `,
   Femke: `
   Hi! Ik ben Femke, midden twintig en ik ben net afgestudeerd aan de master Jeugdliteratuur in Tilburg. Ik kom sinds
-  twee jaar bij DWH/Outsite en de eerste keer dat ik er binnenstapte (bij de KMG), voelde ik me meteen thuis. Voor mij
-  is DWH/Outsite een plek waar iedereen zichzelf kan zijn en waar je superleuke feestjes hebt en gezellig kan kletsen.
+  twee jaar bij DWH en de eerste keer dat ik er binnenstapte (bij de KMG), voelde ik me meteen thuis. Voor mij
+  is DWH een plek waar iedereen zichzelf kan zijn en waar je superleuke feestjes hebt en gezellig kan kletsen.
   Mijn lievelingsactiviteiten zijn toch wel de karaoke- en Just Dance avonden en natuurlijk de Kennismakingsgroep. Ik
-  hoop je snel te ontmoeten, zodat ik je kan laten zien hoe leuk het bij DWH/Outsite is! (:
+  hoop je snel te ontmoeten, zodat ik je kan laten zien hoe leuk het bij DWH is! (:
   `,
   Stefan: `
   Hoi! Ik ben Stefan, 23 jaar en masterstudent in de richting van transport, infrastructuur en logistiek. Ik ben voor
   mijn master verhuisd naar Delft en daarom relatief nieuw hier. Ongeveer een jaar geleden kwam ik voor het eerst bij
-  Outsite. Ik vond het superspannend maar achteraf is het een van de beste dingen die ik ooit heb gedaan. Outsite
-  heeft me niet alleen heel veel goede vrienden opgeleverd, de vereniging heeft me ook enorm geholpen bij het meer
-  mezelf durven te zijn. Ik hoop dat ik als barbuddy ook jou op een laagdrempelige manier kan laten kennismaken met
-  Outsite en haar leden. 
+  DWH. Ik vond het superspannend maar achteraf is het een van de beste dingen die ik ooit heb gedaan. 
+  DWH heeft me niet alleen heel veel goede vrienden opgeleverd, de vereniging heeft me ook enorm geholpen bij 
+  het meer mezelf durven te zijn. Ik hoop dat ik als barbuddy ook jou op een laagdrempelige manier kan laten 
+  kennismaken met DWH en haar leden. 
   `,
   Hidde: `
-  Hey, ik ben Hidde. Ik ben 23 jaar oud en bezig met de master Technische Wiskunde, de kansrekening track. Ik loop nu
-  al ongeveer 3 jaar rond op Outsite, maar ben het laatste jaar een stuk actiever geworden; ik ben vaak vrijwilliger
+  Hey, ik ben Hidde. Ik ben 24 jaar oud en bezig met de master Technische Wiskunde, de kansrekening track. Ik loop nu
+  al ongeveer 3 jaar rond op DWH, maar ben het laatste jaar een stuk actiever geworden; ik ben vaak vrijwilliger
   bij events, zit in verschillende commissies en ben sinds kort nu ook barbuddy. Dit is een enorm verschil met de
-  eerste jaren dat ik bij Outsite kwam. Ik zat toch vaak in mijn eentje en praatte weinig met mensen, omdat ik
+  eerste jaren dat ik bij DWH kwam. Ik zat toch vaak in mijn eentje en praatte weinig met mensen, omdat ik
   niemand echt goed kende. Dit is waar een barbuddy juist goed van pas komt! Als je barbuddy kan ik je aan
-  verschillende mensen van Outsite  voorstellen, waaronder mezelf natuurlijk, en ervoor zorgen dat jij hun leert
+  verschillende mensen van DWH  voorstellen, waaronder mezelf natuurlijk, en ervoor zorgen dat jij hun leert
   kennen en zij jou ook. Dan voelt het bij de volgende keer dat je langskomt niet meer alsof je tussen een groep
   vreemden staat, maar zie je hopelijk wat bekende, gezellige gezichten.
   `,
-  Eva: `
-  Heeyhey, ik ben Eva. Ik ben 20 jaar oud en studeer Industrieel Ontwerp. Naast mijn studie vind ik het erg leuk om
-  saxofoon te spelen en loop ik vaak rond op Outsite. Ik heb hier zelf helemaal mijn plek gevonden en wil ook graag
+  Dante: `
+  Heeyhey, ik ben Dante. Ik ben 21 jaar oud en studeer Industrieel Ontwerp. Naast mijn studie vind ik het erg leuk om
+  saxofoon te spelen en loop ik vaak rond op DWH. Ik heb hier zelf helemaal mijn plek gevonden en wil ook graag
   dat andere mensen zich hier thuis kunnen voelen dus heb ik mezelf opgegeven als barbuddy. Kom gezellig een praatje
-  maken en als je het leuk vind stel ik je ook voor aan een aantal andere Outsiters.
+  maken en als je het leuk vind stel ik je ook voor aan een aantal andere DWHers.
   `
 }

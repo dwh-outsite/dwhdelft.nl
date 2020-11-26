137db84c5a7fd8d9118227da2799246be96b4834
Robert Baart <robert.baart@gmail.com>
2020-11-26 12:55:28 +0100

    Fix barbuddy text line length

--- lang/en_barbuddies.js
+++ lang/en_barbuddies.js
@@ -41,9 +41,9 @@ export default {
   `,
   Govert: `
   Hello I’m Govert, at the start of my thirties. I study architecture and I’m a teacher a high school. I’m a very active
-  volunteer within DWH. In my opinion, DWH stands for a nice and really diverse group of people. A place to organize lots
-  of things and also a place to learn a lot about yourself. I really like meeting new people and introducing them to
-  this awesome association.
+  volunteer within DWH. In my opinion, DWH stands for a nice and really diverse group of people. A place to organize 
+  lots of things and also a place to learn a lot about yourself. I really like meeting new people and introducing them 
+  to this awesome association.
   `,
   Niv: `
   Hey! I’m Niv, 22 yo and studying Applied Physics. In the meantime, I found my place in this cool association (I’m a

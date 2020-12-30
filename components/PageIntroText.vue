125949ea2bf4ffe050a39ed636d4f5e2c0da7edb
Casper Boone <mail@casperboone.nl>
2020-12-30 22:05:43 +0100

    Fix article pages padding

--- components/PageIntroText.vue
+++ components/PageIntroText.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="container mx-auto mb-12 text-xl md:text-2xl leading-normal text-gray-800">
+  <section class="container mx-auto pb-12 text-xl md:text-2xl leading-normal text-gray-800">
     <div class="md:w-2/3 mx-4 md:mx-auto py-8">
       <slot></slot>
     </div>

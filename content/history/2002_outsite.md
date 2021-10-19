8e57963701262abe51b65c2a83c86ddc75df73a9
Casper Boone <mail@casperboone.nl>
2021-10-19 18:24:21 +0200

    Add draft of history page

---
+++ content/history/2002_outsite.md
@@ -0,0 +1,12 @@
+---
+year: 2002
+title_nl: Oprichting Outsite
+title_en: X
+description_nl:
+  In augustus 2002 werd Outsite opgericht als onofficiële ondervereniging en merknaam van DWH gericht
+  op jongeren, en dan vooral studenten. Dit bleek succesvol en via Outsite kreeg DWH, wat zelf
+  ondertussen lang niet alleen uit studenten bestond, nauwere banden met de studentenverenigingen in
+  Delft en de universiteit.
+description_en:
+  X
+---
---
+++ sites/dwhdelft.nl/components/HistoryTimelineEntryYear.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="h-full w-6 flex items-center justify-center">
+      <div class="h-full w-1 bg-brand-300 pointer-events-none"></div>
+    </div>
+    <div
+      class="
+        w-20
+        h-20
+        absolute
+        top-1/2
+        -mt-10
+        -ml-7
+        rounded-full
+        bg-white
+        flex
+        items-center
+        justify-center
+        border-8 border-brand-300
+      "
+    >
+      <span class="text-brand-600 tracking-wider font-bold text-lg" v-text="year" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['year'],
+}
+</script>

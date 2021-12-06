2fc15d50ee4d714eb0ff714d86d2d19528e81cad
Rory Baart <rbaart@gmail.com>
2021-12-06 02:41:16 +0100

    Add resources page

---
+++ sites/dwhdelft.nl/pages/resources.vue
@@ -0,0 +1,208 @@
+<i18n lang="yaml">
+en:
+  title: LGBT+ in Delft
+  titles:
+    local: At home in Delft
+    purple: <strong>PURPLE FRIDAY</strong>
+    sex: Sex-ed 101
+    trans: Being transgender
+    ace: A(ce)-Okay
+    lastig: Dealing with Discrimination?
+  content:
+    intro:
+      - 'It can be hard to find information about queer topics, especially about what is locally available. That’s why we made this page with an overview of both.<br/><br/>'
+    local:
+      - 'Are you a LGBT+ youth under 19 or are you still unsure but want to come into contact with other LGBT+ youth with fun activities? Check out 
+      <a class="font-bold text-purple-600" href="https://instagram.com/jongenoutdelft/">Jong&Out Delft</a>.'
+      - 'Van 16 t/m 28 jaar ben je welkom bij <a class="font-bold text-purple-600" 
+      href="https://dwhdelft.nl/outsite">Outsite</a>, our youth department with a weekly barnight on Thursday, filled with activities and parties. We host a <a class="font-bold text-purple-600" href="https://outsite.nl/kmg">kennismakingsgroep</a> twice a year, where you spend 9 weeks getting to know a group of queer people your age and the queer community.'
+      - 'There is also an active queer nightlife scene. DWH has its own weekly open bar in Delft and there are regular parties. There are lots of trips, organised by and for our members, to Pann, in Utrecht, Popcake 
+      festival or Pride in different cities.'
+    purple:
+      - 'Hi! Nice that you have found our website. The second Friday of December, the 10th to be precise, is Purple Friday. This is a day where we try to convince as many non-queer identifying people as possible to show their support by wearing something purple.'
+      - 'That support is very necessary; we need support so we are not fighting alone. But support does not always have to come in the form of purple clothing. Do you already know how to support queer people around you?'
+      - 'Listen to people from the LGBTQ+ community, and make an effort to understand the problems they face. Speak up when you hear other not-queer people making inappropriate jokes or remarks. Also avoid the use of obscene swear words. Using something like “gay” can already fall in this category.'
+      - 'Respect and practice the pronouns of people around you. Oefen dus met het 
+      gebruik van deze pronouns (zoals die/diens of hen/hun) voor jezelf, zodat als je hen moet aanspreken, 
+      je het automatisch goed doet. Bovenal, verdiep je in het onderwerp! Niets zegt zoveel dat je je best doet als weten waar je het over hebt.'
+    trans:
+      - 'Finding information about being trans can be difficult. Luckily there are a few great information points such as <a class="font-bold text-purple-600" href="https://transvisie.nl">transvisie</a> 
+      and <a class="font-bold text-purple-600" href="https://www.transgenderinfo.nl">transgenderinfo</a>!'
+      - 'It can be very helpful to share experiences and directly ask people questions. In DWH there is a group called Gender Blender, with lots of other not-cis people.'
+    sex:
+      - 'Do you want to learn more about how to have sex as a queer person? The two sites below have lots of great
+      tips to make it enjoyable and safe:'
+      -	'<a class="font-bold text-purple-600" href="https://expreszo.nl/tag/seks/">ExpresZo</a> is a (Dutch) online 
+      queer magazine with some great articles on the basics of queer sex. Check out their other articles as well!'
+      -	'On <a class="font-bold text-purple-600" href="https://sense.info/en">sense.info</a> you can find information
+      about all sorts of topics, from how to have safe sex to how to get tested for STDs and pregnancy.'
+    ace:
+      - 'The Dutch <a class="font-bold text-purple-600" href="https://aseksualiteit.nl">Nederlandse Organisatie 
+      Asexualiteit</a> (NOA) has a lot of information about the asexual spectrum, and links to activities and forums 
+      where you can meet others. Their international equivalent is 
+      <a class="font-bold text-purple-600" href="https://asexuality.org">AVEN</a>.'
+    lastig:
+      - 'Sometimes people give you a hard time because you are queer. In these cases it really helps to report it, and
+      get the support you need.'
+      -	'<a class="font-bold text-purple-600" href="https://www.politie.nl/informatie/contact-met-roze-in-blauw.html">
+      Roze in Blauw Haaglanden</a> is the queer police network of the region. When you need the police non-urgently, you
+      can get in touch with someone through them who will understand you and your situation a bit better.'
+      -	'<a class="font-bold text-purple-600" href="https://stichtingidb.nl/">Stichting Inclusiviteit en Discriminatie Bestrijding</a>
+      (iDb) is the regional organisation mapping and fighting discrimination in Delft and a number of other 
+      municipalities. Even when something is not big enough to go to the police for, it helps to report it to them 
+      anyway so they know where there are issues remaining.'
+nl:
+  title: LHBT+ in Delft
+  titles:
+    local: Thuis in Delft
+    purple: <strong>PAARSE VRIJDAG</strong>
+    sex: Sex-ed 101
+    trans: Transgender zijn
+    ace: A(ce)-Okay
+    lastig: Word je lastig gevallen? 
+  content:
+    intro:
+      - 'Het is soms best lastig om informatie te vinden over queer onderwerpen, en al helemaal om te 
+      vinden wat er lokaal voor je beschikbaar is. Daarvoor hebben we deze pagina gemaakt met een 
+      overzichtje van beide.<br/><br/>'
+    local:
+      - 'Ben jij een LHBT+ jongere onder de 19 jaar of weet je het nog niet precies maar wil je wel in 
+      contact komen met andere LHBT+ jongeren door middel van laagdrempelige activiteiten? Check dan 
+      <a class="font-bold text-purple-600" href="https://instagram.com/jongenoutdelft/">Jong&Out Delft</a>.'
+      - 'Van 16 t/m 28 jaar ben je welkom bij <a class="font-bold text-purple-600" 
+      href="https://dwhdelft.nl/outsite">Outsite</a>, onze jongerenvleugel met een wekelijkse baravond 
+      op donderdag vol activiteiten en feesten. Elk half jaar hebben we voor deze leeftijd ook een 
+      <a class="font-bold text-purple-600" href="https://outsite.nl/kmg">kennismakingsgroep</a>, waar 
+      je in negen weken een groep leeftijdsgenoten en de queer community goed leert kennen.'
+      - 'Er valt hier ook genoeg queer uit te gaan. In Delft zelf is de bar bij DWH elke week open en zijn
+      er regelmatig feesten. Er worden door en voor onze leden vaak ook uitjes naar Pann, in Utrecht, Popcake 
+      festival of Pride in diverse steden georganiseerd.'
+    purple:
+      - 'De tweede vrijdag van december, de 10e om precies te zijn, 
+      is het Paarse Vrijdag. Dit is een dag waarop we proberen om zoveel mogelijk mensen die zich niet 
+      identificeren als queer, te overtuigen hun support te laten zien door iets paars te dragen.'
+      - 'Die support is iets dat hard nodig is; we hebben ondersteuning nodig, zodat wij niet alleen in de 
+      strijd staan. Maar ondersteuning hoeft niet alleen te komen in de vorm van paarse kleding. Weet jij al hoe 
+      je het beste de queer mensen om je heen kan supporten?'
+      - 'Luister naar mensen uit de LHBT+ gemeenschap, en probeer de problemen die ze meemaken te begrijpen.
+      Spreek je uit als je andere niet-queer mensen ongepaste grappen of opmerkingen hoort maken. Vermijd ook het 
+      gebruik van aanstootgevende scheldwoorden. Hier kan iets als “gay” ook al onder vallen.'
+      - 'Respecteer en oefen de voornaamwoorden van de mensen om je heen. In het Nederlands werken de 
+      persoonlijke voornaamwoorden niet altijd voor iedereen even voor de hand liggend. Oefen dus met het 
+      gebruik van deze pronouns (zoals die/diens of hen/hun) voor jezelf, zodat als je hen moet aanspreken, 
+      je het automatisch goed doet. Bovenal, verdiep je in het onderwerp! Niets zegt zoveel dat je je best doet als weten waar je het over hebt.'
+    trans:
+      - 'Het vinden van informatie over trans zijn is soms nog eens best lastig. Gelukkig bestaan er een 
+      aantal handige informatiepunten zoals <a class="font-bold text-purple-600" href="https://transvisie.nl">transvisie</a> 
+      en <a class="font-bold text-purple-600" href="https://www.transgenderinfo.nl">transgenderinfo</a>!'
+      - 'Het kan veel helpen om ervaringen te delen en mensen direct vragen te kunnen stellen. Binnen DWH bestaat 
+      de Gender Blender groep, waar nog veel andere niet-cis mensen in zitten.'
+    sex:
+      - 'Vind meer over hoe je veilig seks kunt hebben – wie je ook bent en op wie je ook valt – op de 
+      volgende paginas:'
+      -	'<a class="font-bold text-purple-600" href="https://expreszo.nl/tag/seks/">ExpresZo</a> is een online 
+      queer magazine waar je veel bruikbare artikelen kunt vinden. Zeker die over seks zijn goed te gebruiken.'
+      -	'Op <a class="font-bold text-purple-600" href="https://sense.info/nl">sense.info</a> vind je alle 
+      informatie die je nodig hebt rondom veilig seks hebben, SOA’s en waar je jezelf kan laten testen, 
+      op SOA ofwel zwangerschap.'
+    ace:
+      - 'Over het aseksuele spectrum en wat het inhoudt kan je veel vinden op de 
+      <a class="font-bold text-purple-600" href="https://aseksualiteit.nl">Nederlandse Organisatie Asexualiteit</a> (NOA).'
+    lastig:
+      - 'Soms zijn er wel eens mensen die moeilijk tegen je doen vanwege je queer zijn. In deze gevallen helpt 
+      het het best om dit te melden. Er zijn verschillende organisaties waarbij je terecht kan.'
+      -	'<a class="font-bold text-purple-600" href="https://www.politie.nl/informatie/contact-met-roze-in-blauw.html">Roze in Blauw Haaglanden</a> 
+      is het queer politienetwerk in de regio. Wanneer je de politie nodig hebt, kun je hier in contact komen 
+      met iemand die jou en je situatie beter begrijpt.'
+      -	'<a class="font-bold text-purple-600" href="https://stichtingidb.nl/">Stichting Inclusiviteit en Discriminatie Bestrijding</a>
+      (iDb) probeert discriminatie van elk soort aard in Delft en 25 andere gemeentes in Noord- en Zuid-Holland 
+      aan te pakken en verminderen; hierbij helpt het heel erg om alles te melden ook als het niet iets is om 
+      mee naar de politie te gaan.'
+</i18n>
+
+<template>
+  <div>
+    <SmallHeader>
+      {{ $t('title') }}
+    </SmallHeader>
+
+    <div class="w-full py-6">
+      <section class="container px-4 mx-auto space-y-6">
+        <div class="md:flex md:space-x-12 space-y-6 md:space-y-0">
+          <div class="md:w-1/2 text-2xl">
+            <p v-for="paragraph in $t('content.intro')" :key="paragraph" v-html="paragraph" />
+            <h2 class="font-medium text-5xl mb-6 leading-tight" v-html="$t('titles.local')" />
+
+            <p
+              v-for="paragraph in $t('content.local')"
+              :key="paragraph"
+              class="text-lg md:text-xl md:leading-relaxed text-gray-800 mb-4"
+              v-html="paragraph"
+            />
+          </div>
+          <div class="flex-1">
+            <div class="md:w-5/6 md:-mt-12 z-50 relative">
+              <div class="bg-paarse-vrijdag-2021 text-white rounded-3xl p-6 w-full text-lg space-y-4">
+                <h2 class="font-semibold text-4xl mb-2" v-html="$t('titles.purple')" />
+                <p v-for="paragraph in $t('content.purple')" :key="paragraph" v-html="paragraph" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+
+    <div class="bg-purple-400 w-full py-6">
+      <section class="container px-4 mx-auto space-y-6">
+        <div class="md:flex md:space-x-12 space-y-6 md:space-y-0">
+          <div class="flex-1">
+            <div class="md:w-5/6 ml-auto">
+              <h2 class="text-white font-semibold text-4xl mb-2 pl-6" v-html="$t('titles.trans')" />
+              <div class="bg-white rounded-3xl p-6 w-full text-lg space-y-4">
+                <p v-for="paragraph in $t('content.trans')" :key="paragraph" v-html="paragraph" />
+              </div>
+            </div>
+            <div class="md:w-5/6 ml-auto mt-8">
+              <h2 class="text-white font-semibold text-4xl mb-2 pl-6" v-html="$t('titles.sex')" />
+              <div class="bg-white rounded-3xl p-6 w-full text-lg space-y-4">
+                <p v-for="paragraph in $t('content.sex')" :key="paragraph" v-html="paragraph" />
+              </div>
+            </div>
+          </div>
+          <div class="flex-1">
+            <div class="md:w-5/6 ml-auto">
+              <h2 class="text-white font-semibold text-4xl mb-2 pl-6" v-html="$t('titles.ace')" />
+              <div class="bg-white rounded-3xl p-6 w-full text-lg space-y-4">
+                <p v-for="paragraph in $t('content.ace')" :key="paragraph" v-html="paragraph" />
+              </div>
+            </div>
+            <div class="md:w-5/6 ml-auto mt-8">
+              <h2 class="text-white font-semibold text-4xl mb-2 pl-6" v-html="$t('titles.lastig')" />
+              <div class="bg-white rounded-3xl p-6 w-full text-lg space-y-4">
+                <p v-for="paragraph in $t('content.lastig')" :key="paragraph" v-html="paragraph" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      showForm: false,
+    }
+  },
+}
+</script>
+
+<style scoped>
+.bg-paarse-vrijdag-2021 {
+  @apply bg-cover;
+  background-image: url(../../../assets/images/purple-friday-2021-bg.svg);
+}
+</style>

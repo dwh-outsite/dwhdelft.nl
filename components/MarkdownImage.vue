b44e94c0595db8b063ab94fd4eb88448e192201d
Casper Boone <mail@casperboone.nl>
2022-02-18 20:26:56 +0100

    Add markdown images support

---
+++ components/MarkdownImage.vue
@@ -0,0 +1,27 @@
+<template>
+  <img :src="loadImage()" :alt="alt" />
+</template>
+
+<script>
+export default {
+  props: {
+    src: {
+      type: String,
+      required: true,
+    },
+    alt: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    loadImage() {
+      try {
+        return require(`#/assets/images/${this.src}`)
+      } catch (error) {
+        return null
+      }
+    },
+  },
+}
+</script>

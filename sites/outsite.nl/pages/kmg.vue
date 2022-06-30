454c6370506a068f373c7083a62eb66928fc61a6
Casper Boone <mail@casperboone.nl>
2022-06-30 14:25:37 +0200

    Fix capitalization

--- sites/outsite.nl/pages/kmg.vue
+++ sites/outsite.nl/pages/kmg.vue
@@ -15,7 +15,7 @@ nl:
     van twee ervaren leden kennis met de vereniging maar vooral ook met elkaar. Op negen donderdagavonden deel je
     met elkaar je coming-out verhalen (als je dat wil), je gaat langs een gay feest en nog veel meer!
   sign_up: Aanmelden voor de KMG
-  next: <strong>De volgende KennisMakingsGroep start op 15 September</strong>.
+  next: <strong>De volgende KennisMakingsGroep start op 15 september</strong>.
   form:
     success: We nemen zo snel mogelijk contact met je op!
 </i18n>

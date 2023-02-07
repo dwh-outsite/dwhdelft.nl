a054c24fe14ec13cad9596ba4ac4ebf644316d0b
Rory Baart <rbaart@gmail.com>
2023-02-07 23:10:02 +0100

    Remove Outsite vertrouwenspersoon page and link to DWH's

--- sites/outsite.nl/components/Footer.vue
+++ sites/outsite.nl/components/Footer.vue
@@ -59,13 +59,13 @@ nl:
                 :name="member.name"
                 :email="member.email"
               />
-              <nuxt-link
-                :to="localePath('confidential-counsellor')"
+              <a
+              href="https://dwhdelft.nl/lets-talk"
                 class="text-white font-bold inline-flex items-center border-b border-white border-dashed"
               >
                 {{ $t('confidential_counsellors') }}
                 <Zondicon icon="arrow-thin-right" class="fill-current w-4 ml-1" />
-              </nuxt-link>
+            </a>
             </div>
             <div>
               <img

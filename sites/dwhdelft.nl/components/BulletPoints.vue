6c700cb509001917121afcfd2b38e106c9c9220b
Laurens Weijs XPS <laurens.weijs@gmail.com>
2023-02-07 16:07:18 +0100

    escaping ' in karaoke + optimizing pictures

--- sites/dwhdelft.nl/components/BulletPoints.vue
+++ sites/dwhdelft.nl/components/BulletPoints.vue
@@ -2,30 +2,30 @@
 en:
   bulletPoints:
     - title: Activism
-      description: 'We are fighting for LGBT+ acceptance in the region of Delft, we do this by speaking to local governments and organisations about their policy, and we participate with national actions for better trans care for LGBT+ refugees.'
+      description: "We are fighting for LGBT+ acceptance in the region of Delft, we do this by speaking to local governments and organisations about their policy, and we participate with national actions for better trans care for LGBT+ refugees."
       image: activism
     - title: Community
-      description: 'From eating with other members to barnights and parties, DWH is the place to be for the queer community to meet each other. Come visit a few times and quickly you know some people.'
+      description: "From eating with other members to barnights and parties, DWH is the place to be for the queer community to meet each other. Come visit a few times and quickly you know some people."
       image: community
     - title: Events
-      description: 'Throughout the year we organise a lot a big events. Karoake's, drag shows and LAN parties for ourselves, but also Purple Friday and since recently Delft Pride, to promote the LGBT+ acceptance under the greater public.'
-        image: events
+      description: "Throughout the year we organise a lot a big events. Karoake's, drag shows and LAN parties for ourselves, but also Purple Friday and since recently Delft Pride, to promote the LGBT+ acceptance under the greater public."
+      image: events
     - title: Support
-      description: 'Next to all the fun activities, we are here to support each other. In our introductory group, young people can safely talk about their coming out stories, in the chats people can exchange advice about dating or their transition.'
+      description: "Next to all the fun activities, we are here to support each other. In our introductory group, young people can safely talk about their coming out stories, in the chats people can exchange advice about dating or their transition."
       image: support
 nl:
   bulletPoints:
     - title: Activisme
-      description: 'Wij zijn voorvechters van LHBT+ rechten in onze regio. We spreken de lokale overheden en organisaties over hun beleid, en doen mee met landelijke acties voor betere trans zorg of opvang van LHBT+ vluchtelingen.'
+      description: "Wij zijn voorvechters van LHBT+ rechten in onze regio. We spreken de lokale overheden en organisaties over hun beleid, en doen mee met landelijke acties voor betere trans zorg of opvang van LHBT+ vluchtelingen."
       image: activism
     - title: Community
-      description: 'Van samen eten en board games tot baravonden en feesten, DWH is de place to be om als queer community elkaar te ontmoeten. Kom een paar keer langs en al snel is er altijd wel iemand die je kent.'
+      description: "Van samen eten en board games tot baravonden en feesten, DWH is de place to be om als queer community elkaar te ontmoeten. Kom een paar keer langs en al snel is er altijd wel iemand die je kent."
       image: community
     - title: Evenementen
-      description: 'Door het jaar heen organiseren we tal van activiteiten. Karaoke's, drag shows en LAN parties voor onszelf, maar ook Paarse Vrijdag en sinds kort Delft Pride, zo promoten we LHBT+ acceptatie onder het grotere publiek.'
+      description: "Door het jaar heen organiseren we tal van activiteiten. Karaoke's, drag shows en LAN parties voor onszelf, maar ook Paarse Vrijdag en sinds kort Delft Pride, zo promoten we LHBT+ acceptatie onder het grotere publiek."
       image: events
     - title: Support
-      description: 'Naast alle leuke activiteiten zijn we er ook om elkaar te steunen. In onze kennismakingsgroep kunnen jongeren veilig hun coming out bespreken, en in de chats wisselen mensen advies uit over daten of hun transitie.'
+      description: "Naast alle leuke activiteiten zijn we er ook om elkaar te steunen. In onze kennismakingsgroep kunnen jongeren veilig hun coming out bespreken, en in de chats wisselen mensen advies uit over daten of hun transitie."
       image: support
 </i18n>
 

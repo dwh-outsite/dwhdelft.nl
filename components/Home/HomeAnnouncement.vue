367f605268cf098fb28fd720970a8ff41228ece8
vanadie <102186388+vanadie@users.noreply.github.com>
2023-08-13 09:46:26 +0200

    please linter

--- components/Home/HomeAnnouncement.vue
+++ components/Home/HomeAnnouncement.vue
@@ -12,7 +12,7 @@ nl:
     <div
       class="bg-white rounded shadow-xl border-2 border-brand-450 hover:bg-brand-450 hover:text-white text-lg md:text-xl text-brand-450 p-4 w-full z-20 md:w-auto md:inline-flex items-center relative"
     >
-      <img src="../../assets/images/photos/vrijwilligersprijs/nvp2023.png" class="w-24 absolute bottom-4 " />
+      <img src="../../assets/images/photos/vrijwilligersprijs/nvp2023.png" class="w-24 absolute bottom-4" />
       <div class="ml-24 pl-4">
         <h2 class="font-bold leading-tight" v-text="$t('description')" />
         <p class="text-sm">{{ $t('read_more') }} &raquo;</p>

37824e18c5a8e76d2be9db1e87a8e56a34946937
Rory Baart <rbaart@gmail.com>
2023-08-28 15:20:56 +0200

    Update barbuddy site text to be a bit more generic than Thu/Sat visits

--- sites/dwhdelft.nl/pages/barbuddy.vue
+++ sites/dwhdelft.nl/pages/barbuddy.vue
@@ -1,17 +1,19 @@
 <i18n lang="yaml">
 en:
   title: Bar Buddies
-  description: If you are looking to come by on a Saturday night but you are not yet familiar with the society you
-    can make use of the bar buddy system! A bar buddy is someone who can not only tell you all there is to know
-    about DWH but they can also introduce you to new people.
+  description: If you are looking to go to one of our weekly activities but you are not yet familiar with the
+    association you can sign up for a bar buddy! A bar buddy is someone who can not only tell you all there is to know
+    about us but will also introduce you to new people. That way, the next time you come by there will be friendly
+    faces waiting!
   sign_up: Sign up for a bar buddy
   action: Find a bar buddy
   bar_buddies_title: Our <strong>Bar Buddies</strong>
 nl:
   title: Barbuddies
-  description: Wil jij een keertje langskomen op een baravond maar ben je bang dat je je niet meteen op je gemak
-    voelt? Meld je dan aan voor een barbuddy! Een barbuddy is iemand die jou de ins en outs kan vertellen over de
-    vereniging, maar ook iemand die jou voor kan stellen aan nieuwe mensen.
+  description: Wil jij een keertje langskomen bij een van onze wekelijkse activiteiten maar ben je bang dat je je niet
+    meteen op je gemak voelt? Meld je dan aan voor een barbuddy! Een barbuddy is iemand die jou de ins en outs kan
+    vertellen over ons, maar ook iemand die jou voor kan stellen aan nieuwe mensen. Zo kom je volgende keer gelijk
+    bekenden tegen!
   action: Vind een barbuddy
   sign_up: Aanmelden voor een barbuddy
   bar_buddies_title: Onze <strong>Barbuddies</strong>
--- sites/outsite.nl/pages/barbuddy.vue
+++ sites/outsite.nl/pages/barbuddy.vue
@@ -1,19 +1,19 @@
 <i18n lang="yaml">
 en:
   title: Bar Buddies
-  description: If you are looking to come by on a bar night but you are not yet familiar with the association
-    you can make use of our bar buddy system! A bar buddy is someone who will meet up with you before the bar night
-    starts and go there with you together. They can introduce you to the people there and tell you anything you want
-    to know about Outsite.
+  description: If you are looking to go to one of our weekly activities but you are not yet familiar with the
+    association you can sign up for a bar buddy! A bar buddy is someone who can not only tell you all there is to know
+    about us but will also introduce you to new people. That way, the next time you come by there will be friendly
+    faces waiting!
   sign_up: Sign up for a bar buddy
   action: Find a bar buddy
   bar_buddies_title: Our <strong>Bar Buddies</strong>
 nl:
   title: Barbuddies
-  description: Wil jij een keertje langskomen op een baravond maar ben je nog niet bekend met de vereniging? Meld
-    je dan aan voor een barbuddy! Een barbuddy is iemand die van tevoren met je afspreekt zodat je samen naar de
-    baravond kunt gaan. Die kan je voorstellen aan de andere bezoekers en alles vertellen wat je maar wilt weten
-    over Outsite.
+  description: Wil jij een keertje langskomen bij een van onze wekelijkse activiteiten maar ben je bang dat je je niet
+    meteen op je gemak voelt? Meld je dan aan voor een barbuddy! Een barbuddy is iemand die jou de ins en outs kan
+    vertellen over ons, maar ook iemand die jou voor kan stellen aan nieuwe mensen. Zo kom je volgende keer gelijk
+    bekenden tegen!
   action: Vind een barbuddy
   sign_up: Aanmelden voor een barbuddy
   bar_buddies_title: Onze <strong>Barbuddies</strong>

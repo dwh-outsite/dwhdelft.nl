a87d9aef1b3ed4e1ae0944dd13c32c507d471ed5
thomcsmits <thomcsmits@outlook.com>
2024-04-13 10:29:23 -0400

    feat: update testimonials outsite (content & component)

--- outsite.nl/pages/testimonials.vue
+++ outsite.nl/pages/testimonials.vue
@@ -19,16 +19,39 @@ const imageOrDefault = (name) => image(name.toLowerCase()) || image('default')
   <LayoutSmallHeader bg="bg-brand-100">{{ t('title') }}</LayoutSmallHeader>
 
   <section class="bg-brand-100 pt-8 pb-16">
-    <ElementsContainer class="grid md:grid-cols-2 gap-8">
-      <PagesTestimonialsTestimonialCard
+    <ElementsContainer class="space-y-8">
+      <ElementsActionCard
         v-for="(testimonial, index) in testimonials"
         :key="testimonial.name"
-        :image="imageOrDefault(testimonial.name)"
-        :name="testimonial.name"
-        :authorDescription="testimonial[`author_description_${locale}`]"
+        headerClass="overflow-hidden"
+        :headerPosition="index % 2 == 0 ? 'left' : 'right'"
+        :title="testimonial.name" 
+        class="bg-white bg-hero-falling-triangles"
+        contentClass="px-16"
       >
-        {{ testimonial[`text_${locale}`] }}
-      </PagesTestimonialsTestimonialCard>
+        <template #header>
+          <img
+            :src="imageOrDefault(testimonial.title)"
+            class="object-cover w-full h-80 md:h-full md:w-60 object-top"
+          />
+        </template>
+
+        <div class="bg-white-gradient-vertical">
+          {{testimonial[`text_${locale}`]}}
+        </div>
+      </ElementsActionCard>
     </ElementsContainer>
   </section>
 </template>
+
+<style scoped>
+.bg-white-gradient-vertical {
+  background: rgb(255, 255, 255);
+  background: linear-gradient(
+    rgba(255, 255, 255, 0) 0%,
+    rgba(255, 255, 255, 1) 15%,
+    rgba(255, 255, 255, 1) 80%,
+    rgba(255, 255, 255, 0) 100%
+  );
+}
+</style>

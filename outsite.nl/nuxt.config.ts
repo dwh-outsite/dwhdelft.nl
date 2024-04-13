039810c9c55396c66a7352f7680c75b8658a2ffd
Casper Boone <mail@casperboone.nl>
2024-04-13 14:58:38 +0200

    Add meta info

--- outsite.nl/nuxt.config.ts
+++ outsite.nl/nuxt.config.ts
@@ -11,4 +11,24 @@ export default defineNuxtConfig({
       },
     },
   },
+  app: {
+    head: {
+      title: 'Outsite - LHBT+ vereniging Delft',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Outsite is er voor jongeren en studenten tot en met 28 jaar die zich identificeren als LHBT+.',
+        },
+        { hid: 'apple-mobile-web-app-title', property: 'apple-mobile-web-app-title', content: 'Outsite' },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+        { rel: 'manifest', href: '/site.webmanifest' },
+        { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#e31c79' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+      ],
+    }
+  },
 })

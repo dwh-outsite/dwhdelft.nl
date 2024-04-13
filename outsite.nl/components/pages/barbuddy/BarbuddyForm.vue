b7cc50e94e4916847a5f9b33fe93c8823f6a5d50
Rory Baart <rbaart@gmail.com>
2024-04-13 16:13:19 +0200

    Add ListboxInput component for selecting multiple options and use it for barbuddies

--- outsite.nl/components/pages/barbuddy/BarbuddyForm.vue
+++ outsite.nl/components/pages/barbuddy/BarbuddyForm.vue
@@ -1,8 +1,8 @@
 <i18n lang="yaml">
 en:
-  success: The bar buddy you selected will contact you as soon as possible.
+  success: A bar buddy you selected will contact you as soon as possible.
 nl:
-  success: Je barbuddy neemt zo snel mogelijk contact met je op.
+  success: Een barbuddy neemt zo snel mogelijk contact met je op.
 </i18n>
 
 <script setup>
@@ -12,19 +12,23 @@ const props = defineProps({
   barBuddies: { type: Array, required: true },
 })
 
-const form = useReMemberForm('barbuddy', {
+const form = useReMemberForm('barbuddy2', {
   name: '',
   age: '',
   language: locale.value === 'nl' ? 'dutch' : 'english',
   email: '',
   phone_number: '',
   pronouns: '',
-  barbuddy: 'no_preference',
+  buddy_preferences: 'no_preference',
+  buddies: [],
   remarks: '',
 })
 
 const formElement = ref(null)
 const submit = async () => {
+  if (form.fields.buddies.length > 0) {
+    form.fields.buddy_preferences = form.fields.buddies.join(', ')
+  }
   await form.submit()
   window.scrollTo({ top: formElement.value.offsetTop, behavior: 'smooth' })
 }
@@ -61,7 +65,7 @@ const barBuddyOptions = Object.fromEntries(props.barBuddies.map((buddy) => [budd
           dutch: $t('forms.label.languages.dutch'),
           english: $t('forms.label.languages.english'),
           no_preference: $t('forms.label.languages.no_preference'),
-        }"
+         }"
       />
     </ElementsFormElement>
 
@@ -70,13 +74,7 @@ const barBuddyOptions = Object.fromEntries(props.barBuddies.map((buddy) => [budd
     </ElementsFormElement>
 
     <ElementsFormElement name="barbuddy" :errors="form.validationErrors">
-      <ElementsFormRadioInput
-        v-model="form.fields.barbuddy"
-        :options="{
-          no_preference: $t('forms.label.languages.no_preference'),
-          ...barBuddyOptions,
-        }"
-      />
+      <ElementsFormListboxInput v-model="form.fields.buddies" :options="barBuddyOptions" />
     </ElementsFormElement>
 
     <ElementsFormElement name="remarks" :errors="form.validationErrors">

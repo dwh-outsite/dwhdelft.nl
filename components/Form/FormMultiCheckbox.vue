d37de89dddf84f70792b440e7829ece5aa680f5a
Rory Baart <rbaart@gmail.com>
2024-04-13 13:50:12 +0200

    Whoops forgot the new file

---
+++ components/Form/FormMultiCheckbox.vue
@@ -0,0 +1,26 @@
+<template>
+  <label class="radio">
+    <input type="checkbox" :value="option" v-model="model" >
+    {{ label }}
+  </label>
+</template>
+
+<script>
+export default {
+  props: {
+    value: [],
+    option: String,
+    label: String,
+  },
+  computed: {
+    model: {
+      get() {
+        return this.value;
+      },
+      set(value) {
+        this.$emit("input", value);
+      },
+    },
+  },
+}
+</script>

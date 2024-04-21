cfc02918df0363fb80800d7b0980afb7deeee7b2
AR Baart <643200+ARBaart@users.noreply.github.com>
2024-04-21 10:32:50 +0200

    Change Andersblad view to past 10 years in text

--- sites/dwhdelft.nl/pages/andersblad.vue
+++ sites/dwhdelft.nl/pages/andersblad.vue
@@ -7,7 +7,7 @@ en:
       The Andersblad presents event and activity reports but also editorials on social topics.
       The magazine is (mostly) in Dutch.
     - The first edition of the Andersblad came out in 1989, as the successor of the DWH maandbrief (a monthly newsletter).
-      Below, we list the editions since 2006.
+      Below, we list the editions of the past ten years.
       Older editions are available upon request.
   list_title: <strong>Andersblad</strong> Editions
 nl:
@@ -17,7 +17,7 @@ nl:
       Het blad wordt gedistribueerd naar leden en maatschappelijke organisaties.
       In het Andersblad worden verslagen van activiteiten en events gedeeld, maar ook opiniestukken over maatschappelijke thema's.
     - Het Andersblad wordt uitgegeven sinds 1989 als opvolger van de DWH maandbrieven.
-      Hieronder zijn de edities sinds 2006 in te zien.
+      Hieronder zijn de edities van de afgelopen tien jaar in te zien.
       Oudere edities zijn op aanvraag ter inzage beschikbaar.
   list_title: <strong>Andersblad</strong> Edities
 </i18n>

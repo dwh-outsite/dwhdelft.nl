75def37ec1e9689f446fd1c4687fcb21135f9dc4
Casper Boone <mail@casperboone.nl>
2024-08-11 11:31:59 +0200

    Add favicon and meta info

--- dwhdelft.nl/nuxt.config.ts
+++ dwhdelft.nl/nuxt.config.ts
@@ -5,14 +5,34 @@ export default defineNuxtConfig({
   extends: '../shared/nuxt.config.ts',
   content: {
     sources: {
-      site: {
-        driver: 'fs',
-        base: resolve(__dirname, 'content')
-      },
       shared: {
         driver: 'fs',
         base: resolve(__dirname, join('..', 'shared', 'content'))
       },
     },
   },
+  app: {
+    head: {
+      title: 'DWH - LHBT+ vereniging Delft',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        {
+          hid: 'description',
+          name: 'description',
+          content:
+            'DWH is dé onafhankelijke LHBT+ vereniging van Delft en omgeving. We zijn twee dingen; een ontmoetingsplek ' +
+            'voor LHBT+ mensen en hun vrienden en een groep die actief strijdt voor meer LHBT+ rechten en acceptatie.',
+        },
+        { hid: 'apple-mobile-web-app-title', property: 'apple-mobile-web-app-title', content: 'DWH Delft' },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+        { rel: 'manifest', href: '/site.webmanifest' },
+        { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#e31c79' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+      ],
+    }
+  }
 })

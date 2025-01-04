c01ede2d288cf11929d55514e5671161ebd07b88
Casper Boone <mail@casperboone.nl>
2025-01-04 17:44:28 +0100

    Add tailwindcss linting for fixed class ordering

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,11 @@
 import dpgRadioVue3 from '@dpgradio/eslint-config-vue3'
+import tailwind from 'eslint-plugin-tailwindcss'
 
 export default [
   {
     ignores: ['**/.nuxt/', '**/.output/'],
   },
   ...dpgRadioVue3,
+  ...tailwind.configs['flat/recommended'],
   { rules: { 'no-undef': 'off' } },
 ]

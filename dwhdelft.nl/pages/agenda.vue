ac7c2269027504ff70dd2cbd452e7f7812b2cdb0
thomcsmits <thomcsmits@outlook.com>
2025-04-25 16:25:12 -0400

    start of agenda page

---
+++ dwhdelft.nl/pages/agenda.vue
@@ -0,0 +1,23 @@
+<i18n lang="yaml">
+en: 
+  title: Agenda
+  intro: Stay up to date with our activities and events.
+nl: 
+  title: Agenda
+  intro: Blijf op de hoogte van onze activiteiten en evenementen.
+</i18n>
+
+<script setup>
+const { t } = useT()
+</script>
+
+<template>
+  <LayoutSmallHeader>
+    {{ t('title') }}
+  </LayoutSmallHeader>
+
+  <LayoutPageIntroText>
+    <p v-text="t('intro')" />
+  </LayoutPageIntroText>
+
+</template>

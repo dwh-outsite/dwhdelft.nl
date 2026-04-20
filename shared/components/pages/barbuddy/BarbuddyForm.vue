f696f1eb6f457c925e6d53bc25cc4abf3e5b4773
A.R. Baart <rory@paxx.nl>
2026-04-20 14:18:44 +0200

    Simplify barbuddy form submit logic

--- shared/components/pages/barbuddy/BarbuddyForm.vue
+++ shared/components/pages/barbuddy/BarbuddyForm.vue
@@ -25,11 +25,7 @@ const form = useReMemberForm('barbuddy', {
 
 const formElement = ref(null)
 const submit = async () => {
-  let { buddies, ...fields } = form.fields
-  await form.submit({
-    ...fields,
-    barbuddy: buddies.length ? buddies.join(', ') : 'no_preference',
-  })
+  await form.submit({ barbuddy: form.fields.buddies.join(', ') || 'no_preference' })
   window.scrollTo({ top: formElement.value.offsetTop, behavior: 'smooth' })
 }
 
